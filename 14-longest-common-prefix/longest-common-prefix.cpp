08099d9ab680a3b14c76a7d90f27a96c81b54934
Shivam Singh <039aspire2025@gmail.com>
2025-11-10 22:59:35 +0530

    Time: 3 ms (11.43%) | Memory: 18 MB (5.10%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string ans;
+        int i = 0;
+        while (true) {
+            char cur_ch = 0;
+            for (auto str : strs) {
+                if (i >= str.size()) {
+                    cur_ch = 0;
+                    break;
+                }
+                if (cur_ch == 0) {
+                    cur_ch = str[i];
+                } else if (str[i] != cur_ch) {
+                    cur_ch = 0;
+                    break;
+                }
+            }
+            if (cur_ch == 0) {
+                break;
+            }
+            ans.push_back(cur_ch);
+            i++;
+        }
+        return ans;
+    }
+};

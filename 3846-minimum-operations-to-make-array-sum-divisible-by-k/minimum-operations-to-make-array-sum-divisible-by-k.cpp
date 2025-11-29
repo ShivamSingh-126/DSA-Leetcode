a58672f4269ee3cb09fa8546f1dc6b402b89d626
Shivam Singh <039aspire2025@gmail.com>
2025-11-29 15:57:55 +0530

    Time: 0 ms (100.00%) | Memory: 45.4 MB (35.30%) - LeetSync

--- 3846-minimum-operations-to-make-array-sum-divisible-by-k/minimum-operations-to-make-array-sum-divisible-by-k.cpp
+++ 3846-minimum-operations-to-make-array-sum-divisible-by-k/minimum-operations-to-make-array-sum-divisible-by-k.cpp
@@ -5,9 +5,11 @@ public:
         for (int x : nums) {
             sum += x;
         }
-        if (sum % k == 0)
-            return 0;
-        count = sum % k;    //remainder tell how min operation we have to do to divisible by k
-        return count;
+        // if (sum % k == 0)
+        //     return 0;
+        // count = sum % k;    //remainder tell how min operation we have to do
+        // to divisible by k return count;
+
+        return sum % k;
     }
 };

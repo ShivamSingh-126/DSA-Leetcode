3b05a71eb1908649e6633643884713c839746188
Shivam Singh <039aspire2025@gmail.com>
2025-12-14 09:00:23 +0530

    Time: 6 ms (63.90%) | Memory: 7.9 MB (54.57%) - LeetSync

--- 2037-count-square-sum-triples/count-square-sum-triples.cpp
+++ 2037-count-square-sum-triples/count-square-sum-triples.cpp
@@ -2,14 +2,27 @@ class Solution {
 public:
     int countTriples(int n) {
         //  Brute force
-        int count = 0;
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j < n; j++) {
-                for (int c = 1; c <= n; c++) {
-                    if (i * i + j * j == c * c) {
-                        count++;
-                    }
-                }
+        // int count = 0;
+        // for (int i = 1; i < n; i++) {
+        //     for (int j = 1; j < n; j++) {
+        //         for (int c = 1; c <= n; c++) {
+        //             if (i * i + j * j == c * c) {
+        //                 count++;
+        //             }
+        //         }
+        //     }
+        // }
+        // return count;
+
+        // Optimal solution
+        int count=0;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                int sum=i*i+j*j;
+                int check=sqrt(sum);
+                if(check<=n && check*check==sum) count++;
             }
         }
         return count;

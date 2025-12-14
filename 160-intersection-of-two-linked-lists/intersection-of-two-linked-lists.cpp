cfa5e5f64ec7a6aa9ad5477add54f970fce84cda
Shivam Singh <039aspire2025@gmail.com>
2025-12-14 13:02:35 +0530

    Time: 72 ms (10.05%) | Memory: 29.7 MB (5.33%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -9,15 +9,33 @@
 class Solution {
 public:
     ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
-        if (!headA || !headB)
-            return NULL;
+        // brute Force Approach
+        // if (!headA || !headB)
+        //     return NULL;
 
-        ListNode* tempA = headA;
-        ListNode* tempB = headB;
-        while (tempA != tempB) {
-            tempA = (tempA == NULL) ? headB : tempA->next;
-            tempB = (tempB == NULL) ? headA : tempB->next;
+        // ListNode* tempA = headA;
+        // ListNode* tempB = headB;
+        // while (tempA != tempB) {
+        //     tempA = (tempA == NULL) ? headB : tempA->next;
+        //     tempB = (tempB == NULL) ? headA : tempB->next;
+        // }
+        // return tempA;
+
+        // Hashing Method
+        ListNode* temp;
+        map<ListNode*, int> mpp;
+
+        temp = headA;
+        while (temp != NULL) {
+            mpp[temp] = 1;
+            temp = temp->next;
         }
-        return tempA;
+        temp = headB;
+        while (temp != NULL) {
+            if (mpp.find(temp) != mpp.end())
+                return temp;
+            temp = temp->next;
+        }
+        return NULL;
     }
 };

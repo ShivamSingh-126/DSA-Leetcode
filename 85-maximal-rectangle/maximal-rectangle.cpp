8f1e5fdc2c8099a9d4bd729668228a447a154cb2
Shivam Singh <039aspire2025@gmail.com>
2026-02-13 12:47:23 +0530

    Time: 19 ms (22.65%) | Memory: 21.2 MB (15.72%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.cpp
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -1,5 +1,5 @@
 class Solution {
-public:
+public:/*
     int largestRectangleArea(vector<int>& histo) {
         stack<int> st;
         int maxA = 0;
@@ -21,6 +21,45 @@ public:
             st.push(i);
         }
         return maxA;
+    }
+    */
+    int largestRectangleArea(vector<int>& heights) 
+    {
+        int n=heights.size();
+        if(n==1)  return heights[0];
+        vector<int> left(n,0);
+        vector<int> right(n,0);
+        stack<int> st;
+
+        // right Arrays 
+        for(int i=n-1;i>=0;i--)
+        {
+            while(!st.empty() && heights[st.top()] >= heights[i])
+            {
+                st.pop();
+            }
+            right[i]=st.empty() ? n : st.top();
+            st.push(i);
+        }
+        while(!st.empty()) st.pop();
+        // left array
+        for(int i=0;i<n;i++)
+        {
+            while(!st.empty() && heights[st.top()] >= heights[i])
+            {
+                st.pop();
+            }
+            left[i]=st.empty() ? -1 : st.top();
+            st.push(i);
+        }
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            int width=right[i]-left[i]-1;
+            int curarea=heights[i] * width;
+            ans=max(ans,curarea);
+        }
+        return ans;
     }
     int maximalRectangle(vector<vector<char>>& matrix) 
     {

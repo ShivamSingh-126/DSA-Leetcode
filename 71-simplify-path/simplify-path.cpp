8bff571a1f693121ba116cf71446406831151209
Shivam Singh <039aspire2025@gmail.com>
2026-02-17 21:22:39 +0530

    Time: 8 ms (10.92%) | Memory: 23.7 MB (5.03%) - LeetSync

--- 71-simplify-path/simplify-path.cpp
+++ 71-simplify-path/simplify-path.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     string simplifyPath(string path) 
     {
+        /*
+        // Using stack
         string token="";
         stringstream sp(path);
         stack<string> st;
@@ -28,6 +30,36 @@ public:
         {
             res="/"+ st.top() + res;
             st.pop();
+        }
+        return res;
+        */
+        
+        // Using Vector
+        string token="";
+        stringstream sp(path);
+        vector<string> st;
+
+        while(getline(sp,token,'/'))
+        {
+            if(token == "" || token ==".")
+            {
+                continue;
+            }
+            if(token != ".." )
+            {
+               st.push_back(token);
+            }
+            else if(!st.empty())
+            {
+                st.pop_back();
+            }
+        }
+        if(st.empty())  return "/";
+
+        string res="";
+        for(auto &token : st)
+        {
+            res = res + "/" + token;
         }
         return res;
     }

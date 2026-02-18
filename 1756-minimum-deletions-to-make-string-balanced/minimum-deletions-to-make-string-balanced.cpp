c452b38e0ebf165475f43635f0aebe593ddafa91
Shivam Singh <039aspire2025@gmail.com>
2026-02-18 19:28:28 +0530

    Time: 32 ms (38.57%) | Memory: 25.4 MB (95.90%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -2,8 +2,9 @@ class Solution {
 public:
     int minimumDeletions(string s) 
     {
-        int count=0;
         int n=s.size();
+        /*
+        int count=0;
         stack<char> st;
         for(int i=0;i<n;i++)
         {
@@ -17,6 +18,30 @@ public:
                 st.push(ch);
             }
         }
+        return count;
+        */
+
+        int counta=0;
+        if (n == 0) return 0;
+        for(int i=n-1;i>=0;i--)
+        {
+            if(s[i]=='a')
+            {
+                counta++;
+            }
+        }
+        int count=INT_MAX;
+        int countb=0;
+
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]=='a')  counta--;
+            
+            count=min(count,counta+countb);
+
+            if(s[i]=='b') countb++;
+        }
+
         return count;
     }
 };

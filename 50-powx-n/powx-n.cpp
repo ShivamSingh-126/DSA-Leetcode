32b89c09fd20a536691bba0b4f69e08e420654a1
Shivam Singh <039aspire2025@gmail.com>
2026-02-27 16:38:17 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (14.32%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,5 +1,13 @@
 class Solution {
 public:
+    double solve(double x, long long n)
+    {
+        if( n== 0) return 1;
+        double val=solve(x,n/2);
+        if(n%2==0)  return val*val;
+
+        return  x*val*val;
+    }
     double myPow(double x, int n) 
     {
         /*
@@ -7,6 +15,7 @@ public:
         ans=pow(x,n);
         return ans;
         */
+        /*
         long long a=n;
         double ans=1.0;
         if (a < 0) 
@@ -27,6 +36,15 @@ public:
                 a=a/2;
             }            
         }
-        return ans;        
+        return ans;
+        */   
+        long long N=n;
+
+        if(N < 0)
+        {
+            x=1/x;
+            N=-N;
+        }  
+        return solve(x,N);
     }
 };

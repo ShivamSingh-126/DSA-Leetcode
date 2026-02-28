42dadb0fd28bbb8101e180a04a5f50efff52882f
Shivam Singh <039aspire2025@gmail.com>
2026-02-28 16:58:48 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (50.05%) - LeetSync

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.cpp
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int minSteps(string s, string t) 
     {
+        /*
         int n=s.length();
         int maps[26]={0};
         int mapt[26]={0};
@@ -18,6 +19,24 @@ public:
         {
             if(maps[i] > mapt[i])
             ans+=maps[i]-mapt[i];
+        }
+        return ans;
+        */
+        int n=s.length();
+        int mp[26]={0};
+
+        for(int i=0;i<n;i++)
+        {
+            mp[s[i]-'a']++;
+            mp[t[i]-'a']--;
+        }
+
+        int ans=0;
+
+        for(int i=0;i<26;i++)
+        {
+            if(mp[i] > 0)
+            ans+=mp[i];
         }
         return ans;
     }

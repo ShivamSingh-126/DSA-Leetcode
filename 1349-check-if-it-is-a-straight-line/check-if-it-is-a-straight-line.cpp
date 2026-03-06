2fa5b584db2905fd3277e5e28f602c8278bf2c05
Shivam Singh <039aspire2025@gmail.com>
2026-03-06 11:10:30 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (46.95%) - LeetSync

---
+++ 1349-check-if-it-is-a-straight-line/check-if-it-is-a-straight-line.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool checkStraightLine(vector<vector<int>>& coordinates) 
+    {
+        int n=coordinates.size();
+        //    [[1,2],[2,3],[3,4],[4,5],[5,6],[6,7]]
+
+        int dy=coordinates[1][1] - coordinates[0][1];
+        int dx=coordinates[1][0] - coordinates[0][0];
+
+        for(int i=2;i<n;i++)
+        {
+            int dyi=coordinates[i][1] - coordinates[0][1];
+            int dxi=coordinates[i][0] - coordinates[0][0];
+
+            // slope of straight line is same so we check slope
+
+            if(dyi * dx != dxi * dy)  return false;
+        }
+        return true;
+    }
+};

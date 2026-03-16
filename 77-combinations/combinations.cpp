3f25bd5991424aead7c2826bf5cb30bdd8bc5027
Shivam Singh <039aspire2025@gmail.com>
2026-03-16 18:19:42 +0530

    Time: 37 ms (72.90%) | Memory: 62.6 MB (57.61%) - LeetSync

--- 77-combinations/combinations.cpp
+++ 77-combinations/combinations.cpp
@@ -7,14 +7,21 @@ public:
             ans.push_back(temp);
             return;
         }
-        if(start > n)   return;
-
+        // if(start > n)   return;
+/*
         //include
         temp.push_back(start);
         solve(n,k-1,ans,temp,start+1);
         //exclude
         temp.pop_back();
         solve(n,k,ans,temp,start+1);
+        */
+        for(int i=start;i<=n;i++)
+        {
+            temp.push_back(i);
+            solve(n,k-1,ans,temp,i+1);
+            temp.pop_back();
+        }
     }
     vector<vector<int>> combine(int n, int k) 
     {

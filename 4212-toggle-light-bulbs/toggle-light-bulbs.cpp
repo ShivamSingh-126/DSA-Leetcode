19369923627b0e4b6b9c7bb73df8de26a61ace55
Shivam Singh <039aspire2025@gmail.com>
2026-03-16 10:03:15 +0530

    Time: 0 ms (100.00%) | Memory: 33.6 MB (76.44%) - LeetSync

--- 4212-toggle-light-bulbs/toggle-light-bulbs.cpp
+++ 4212-toggle-light-bulbs/toggle-light-bulbs.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     vector<int> toggleLightBulbs(vector<int>& bulbs) 
     {
+        /*
         // sort(bulbs.begin(),bulbs.end());
         unordered_set<int>st;
 
@@ -16,6 +17,19 @@ public:
         vector<int> ans(st.begin(),st.end());
         sort(ans.begin(),ans.end());
         return ans;
-        
+        */
+        vector<int>st(101,0);
+
+        for(auto val : bulbs)
+        {
+            st[val] ^=1;
+        }
+        vector<int> ans;
+        for(int i=0;i<101;i++)
+        {
+            if(st[i])
+            ans.push_back(i);
+        }
+        return ans;
     }
 };

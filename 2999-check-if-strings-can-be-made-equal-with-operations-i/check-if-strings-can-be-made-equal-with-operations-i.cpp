80e93113fb968d69d05823ff74e4c24a49116ff5
Shivam Singh <039aspire2025@gmail.com>
2026-03-30 23:18:41 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (28.21%) - LeetSync

--- 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.cpp
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -2,10 +2,36 @@ class Solution {
 public:
     bool canBeEqual(string s1, string s2) 
     {
+        /*
         bool cond1 = (s1[0] == s2[0] && s1[2] == s2[2]) ||  (s1[0] == s2[2] && s1[2] == s2[0]);
 
         bool cond2 = (s1[1] == s2[3] && s1[3] == s2[1]) ||  (s1[1] == s2[1] && s1[3] == s2[3]);
 
         return cond1 && cond2;
+        */
+        int even[26] = {0};
+        int odd[26] = {0};
+
+        for(int i=0;i<4;i++)
+        {
+            if(i%2==0)
+            {
+                even[s1[i] - 'a']++;
+                even[s2[i] - 'a']--;
+            }
+            else
+            {
+                odd[s1[i] - 'a']++;
+                odd[s2[i] - 'a']--;
+            }
+        }
+        for(int i=0;i<26;i++)
+        {
+            if(even[i] != 0 || odd[i] != 0)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 };

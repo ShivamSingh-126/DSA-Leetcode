7160439b5512666462ded3b22d74f3dee07e3a05
Shivam Singh <039aspire2025@gmail.com>
2026-04-05 14:36:30 +0530

    Time: 3 ms (46.40%) | Memory: 10.3 MB (47.26%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int u=0;
+        int l=0;
+        for (char ch : moves)
+        {
+            if(ch == 'U') u++;
+            if(ch == 'D') u--;
+            if(ch == 'L') l++;
+            if(ch == 'R') l--;
+        }
+        return u == 0 && l == 0 ? true : false; 
+    }
+};

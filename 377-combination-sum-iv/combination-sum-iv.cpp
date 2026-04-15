93290797f96462cbd3c05cf3b6f93c4e3686c2a0
Shivam Singh <039aspire2025@gmail.com>
2026-04-15 10:30:46 +0530

    Time: 5 ms (5.79%) | Memory: 9.8 MB (8.43%) - LeetSync

---
+++ 377-combination-sum-iv/combination-sum-iv.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int t[201][1001];
+    int solve(vector<int>& nums,int idx, int target)
+    {        
+
+        if(target == 0 )
+        {
+            return 1;            
+        }
+        if(idx >= nums.size() || target < 0)
+        {
+            return 0;
+        }
+        if(t[idx][target] != -1)
+        {
+            return t[idx][target];
+        }
+
+        int taken_idx=solve(nums,0,target-nums[idx]);
+        int rej_idx=solve(nums,idx+1,target);
+
+        return t[idx][target]=taken_idx+rej_idx;
+    }
+    int combinationSum4(vector<int>& nums, int target) 
+    {   
+        memset(t,-1,sizeof(t));
+        return solve(nums,0,target);
+    }
+};

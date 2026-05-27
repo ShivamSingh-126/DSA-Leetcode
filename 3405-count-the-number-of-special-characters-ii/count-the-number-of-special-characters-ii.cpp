0a03c2c779e776254bada9e5c124679a73c19d1a
Shivam Singh <039aspire2025@gmail.com>
2026-05-27 19:36:04 +0530

    Time: 60 ms (93.26%) | Memory: 24.4 MB (53.09%) - LeetSync

---
+++ 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int numberOfSpecialChars(string word) 
+    {
+        int n=word.size();
+        vector<int>fstcap(26,-1);
+        vector<int>lstsml(26,-1);
+
+        for(int i=0;i<n;i++)
+        {
+            char ch=word[i];
+
+            if(islower(ch))
+            {
+                lstsml[ch-'a']=i;
+            }
+            else
+            {
+                if(fstcap[ch-'A'] == -1)
+                {
+                    fstcap[ch-'A']=i;
+                }
+            }
+        }
+        int count=0;
+        for(int i=0;i<26;i++)
+        {
+            if(lstsml[i] !=-1 && fstcap[i] != -1 && lstsml[i] < fstcap[i])
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+};

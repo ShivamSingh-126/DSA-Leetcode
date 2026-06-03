66d013bf6007b8d60e1a6ad70bf2b2414f3dc227
Shivam Singh <039aspire2025@gmail.com>
2026-06-03 08:17:03 +0530

    Time: 7 ms (64.53%) | Memory: 12.6 MB (38.61%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -23,6 +23,7 @@ public:
         }
         return maxi;
         */
+        /*
         vector<int> hash(256,-1);
         int l=0,r=0,maxlen=0,n=s.length();
         while(r<n)
@@ -40,5 +41,33 @@ public:
             r++;
         }
         return maxlen;
+        */
+
+        
+        unordered_map<char,int>mpp;
+        int low=0,res=0,n=s.length();
+
+        for(int high=0;high<n;high++)
+        {
+            mpp[s[high]]++;
+
+            int k=high-low+1;
+            while(mpp.size() < k)
+            {
+                mpp[s[low]]--;
+
+                if(mpp[s[low]] == 0)
+                {
+                    mpp.erase(s[low]);
+                }
+                low++;
+                k=high-low+1;
+            }
+            if(mpp.size()==k)
+            {
+                res=max(res,high-low+1);
+            }
+        }
+        return res;
     }
 };

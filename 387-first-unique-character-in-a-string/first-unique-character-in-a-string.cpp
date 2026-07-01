acb961336ae6c314d570d8eac773119d9c9b76f7
Shivam Singh <039aspire2025@gmail.com>
2026-07-01 12:19:00 +0530

    Time: 16 ms (30.63%) | Memory: 15 MB (20.76%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int firstUniqChar(string s) 
     {
+        /*
         unordered_map<char,int>mpp;
         queue<int>Q;
 
@@ -17,5 +18,20 @@ public:
             Q.pop();
         }
         return Q.empty() ? -1:Q.front();
+        */
+        unordered_map<char,int>mpp;
+        int n=s.size();
+        for(int i=0;i<n;i++)
+        {
+            mpp[s[i]]++;
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(mpp[s[i]] == 1)
+            {
+                return i;
+            }
+        }
+        return -1;
     }
 };

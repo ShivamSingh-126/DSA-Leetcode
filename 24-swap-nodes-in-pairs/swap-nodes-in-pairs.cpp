d80179cd33fb46e918664d183a1066f349753806
Shivam Singh <039aspire2025@gmail.com>
2026-07-04 07:33:04 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (86.50%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
@@ -10,8 +10,28 @@
  */
 class Solution {
 public:
+#define null NULL
+#define node ListNode
+#define data val
+
+void reverse(node* head,int times)
+{
+    node* curr=head;
+    node*prev=null;
+
+    while(times--)
+    {
+        node* nex=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=nex;
+    }
+    return;
+}
+
     ListNode* swapPairs(ListNode* head) 
     {
+        /*
         if(head==NULL || head->next == NULL)  return head;
 
         ListNode* temp=head->next;
@@ -19,5 +39,50 @@ public:
         temp->next=head;
 
         return temp;
+        */
+
+        if(head == null)
+        return null;
+
+        node* left=head;
+        node* right;
+        node* res=null;
+        node* prevleft=null;
+        int size=2;
+        while(true)
+        {
+            right=left;
+            for(int i=0;i<size-1;i++)
+            {
+                if(right==null)
+                break;
+                right=right->next;
+            }
+            if(right)
+            {
+                node* nexleft=right->next;
+                reverse(left,size);
+                if(prevleft)
+                {
+                    prevleft->next=right;
+                }
+                prevleft=left;
+                if(res==null)
+                res=right;
+
+                left=nexleft;
+            }
+            else
+            {
+                if(prevleft)
+                prevleft->next=left;
+                if(res==null)
+                res=left;
+
+                break;
+            }
+        }
+        return res;
+
     }
 };

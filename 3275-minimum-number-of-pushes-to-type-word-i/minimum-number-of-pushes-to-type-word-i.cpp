7a64d50a1d2428f301162feb20aed1518d5a295d
Shivam Singh <039aspire2025@gmail.com>
2026-07-30 15:41:15 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (54.67%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int minimumPushes(string word) 
     {
+        /*
         int ans=0;
         unordered_map<int,int>mpp;  // 2 to 9
         int keys=2;
@@ -15,6 +16,25 @@ public:
             ans +=mpp[keys];
 
             keys++;
+        }
+        return ans;
+        */
+        int ans=0;
+        vector<int>mpp(26,0);  
+
+        for(char &ch : word)
+        {
+            mpp[ch-'a']++;
+        }
+        sort(mpp.begin(),mpp.end(),greater<int>());
+
+        for(int i=0;i<26;i++)
+        {
+            int freq = mpp[i];
+
+            int cost = i/8+1;
+
+            ans +=cost * freq;
         }
         return ans;
     }
